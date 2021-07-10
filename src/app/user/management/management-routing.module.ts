b2705fd46c719c9b203637adf8a88634690917f8
Tales <not_functional@a.com>
2021-07-09 21:25:52 -0300

    adição de template para listagem de usuários

--- src/app/user/management/management-routing.module.ts
+++ src/app/user/management/management-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserResolver } from 'src/app/resolver/user.resolver';
 import { ManagementComponent } from './management.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ManagementComponent
+    component: ManagementComponent,
+    resolve: { page: UserResolver }
   }
 ];
 
