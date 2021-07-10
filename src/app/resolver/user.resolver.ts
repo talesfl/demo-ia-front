46c71d550aee30105ca2a1ef3dca6c45b01ed048
Tales <not_functional@a.com>
2021-07-09 21:25:23 -0300

    Implementação de user resolver

--- src/app/resolver/user.resolver.ts
+++ src/app/resolver/user.resolver.ts
@@ -1,3 +1,28 @@
-export class UserResolver {
+import { Injectable } from '@angular/core';
+
+import {
+    Resolve,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot
+} from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { Page } from '../domain/page';
+import { User } from '../domain/user';
+import { UserService } from '../service/user.service';
+
+@Injectable()
+export class UserResolver implements Resolve<Page<User>> {
+
+    constructor(
+        private userService: UserService
+    ) {}
+
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Page<User> | Observable<Page<User>> | Promise<Page<User>> {
+        return this.userService.findByNomeStartingWith();
+    }
 
 }
