235e6fbc1be90f8c6f8c0dd9a07df17959f8ff1e
Tales <not_functional@a.com>
2021-07-12 19:49:00 -0300

    adição do id do usuário

--- src/app/resolver/email.resolver.ts
+++ src/app/resolver/email.resolver.ts
@@ -11,20 +11,22 @@ import { Observable } from 'rxjs';
 import { Page } from '../domain/page';
 import { Email } from '../domain/email';
 import { EmailService } from '../service/email.service';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Injectable()
 export class EmailResolver implements Resolve<Page<Email>> {
 
     constructor(
-        private emailService: EmailService
+        private emailService: EmailService,
+        private authenticationService: AuthenticationService
     ) {}
 
     resolve(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Page<Email> | Observable<Page<Email>> | Promise<Page<Email>> {
         
-        // TODO: buscar da sessão o id do usuário logado
-        return this.emailService.findByUserFromId(0);
+        const userId = this.authenticationService.loggedUser.id;
+        return this.emailService.findByUserFromId(userId);
     }
 
 }
